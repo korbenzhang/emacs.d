f6eed9511f40189fc32176ad3a9c6fe03a992a47
Korben Zhang <korbenzhang@gmail.com>
2015-07-02 22:11:25 +0800

    update

--- init.el
+++ init.el
@@ -1,5 +1,23 @@
 ;; author Korben Zhang
 
+(require 'init-modeline)
+(require 'cl-lib)
+(require 'init-compat)
+(require 'init-utils)
+(require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
+
+
+(require 'idle-require)
+(require 'init-elpa)
+
+(require 'init-xterm)
+
+;; my personal setup, other major-mode specific setup need it.
+;; It's dependent on init-site-lisp.el
+(if (file-exists-p "~/.custom.el") (load-file "~/.custom.el"))
+
+
+
 (setq inhibit-startup-message t) ;关闭启动画面
 
 (show-paren-mode t)
@@ -18,7 +36,27 @@
 (set-locale-environment "UTF-8")
 (set-default-font "Consolas 16")
 
+(menu-bar-mode nil)
 (tool-bar-mode nil)
+(scroll-bar-mode nil)
+
+(display-time-mode 1)
+(setq display-time-24hr-format t)
+(setq display-time-day-and-date t) 
+
+
+(fset 'yes-or-no-p 'y-or-n-p) 
+(auto-compression-mode 1)   
+
+(show-paren-mode 1)
+(setq visible-bell nil)
+
+(setq inhibit-startup-message t)   
+(setq gnus-inhibit-startup-message t)   
+
+(setq x-select-enable-clipboard t)  
+
+
 
 
 
