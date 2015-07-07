6d959f3b781b843ddc13350af7f3164c8a9db0bd
Korben Zhang <korbenzhang@gmail.com>
2015-07-07 11:25:24 +0800

    update font size

--- ext/theme.el
+++ ext/theme.el
@@ -41,6 +41,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Monaco" :foundry "apple" :slant normal :weight normal :height 131 :width normal))))
+ '(default ((t (:family "Monaco" :foundry "apple" :slant normal :weight normal :height 111 :width normal))))
  '(window-numbering-face ((t (:foreground "DeepPink" :underline "DeepPink" :weight bold))) t))
 
