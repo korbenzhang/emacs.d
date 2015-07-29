c244055fce465c73f0112fcd95401855ac59e76e
Korben Zhang <korbenzhang@gmail.com>
2015-07-29 18:54:58 +0800

    fix ac

--- lisp/init-golang.el
+++ lisp/init-golang.el
@@ -2,7 +2,7 @@
 
 (require 'go-mode)
 (require 'go-errcheck)
-;;(require 'go-eldoc) ;; Don't need to require, if you install by package.el
+(require 'go-eldoc) ;; Don't need to require, if you install by package.el
 
 (add-hook 'go-mode-hook 'go-eldoc-setup)
 
