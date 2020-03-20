0dfa42ba51d9db8577878bd021d70df111a9bf7e
Korben Zhang <korbenzhang@gmail.com>
2020-03-21 02:00:06 +0800

    fix

--- init.el
+++ init.el
@@ -5,6 +5,16 @@
 ;;----------------------------------------------------------------------------
 
 ;; emacs init essential
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+                         ("melpa" . "http://elpa.emacs-china.org/melpa/")))
+
+(package-initialize)
+
 (load "~/.emacs.d/init_ess.el")
 
 ;; emacs init full
